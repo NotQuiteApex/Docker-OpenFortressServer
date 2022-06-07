ca88617d6747579e2da59575dfe4e8fe2ab0c88a
Logan H-D <notquiteapex@gmail.com>
2022-06-06 22:41:06 -0400

    carraige returns suck

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
-# Runs the server itself. Configure any server specific settings here.
-# Also updates the server whenever the server shuts down cleanly.
-while :
+!/bin/sh
+ Runs the server itself. Configure any server specific settings here.
+ Also updates the server whenever the server shuts down cleanly.
+hile :
+o
+echo "Starting Server."
+./srcds_run -console -game open_fortress \
+	-insecure -timeout 0 -nohltv -autoupdate \
+	-steam_dir /root/server/ -steamcmd_script /root/tf2sdk-update.txt \
+	|| exit 1
+echo "Server exited."
+
+echo "Updating Open Fortress"
+until ~/of-update.sh
 do
-	echo "Starting Server."
-	./srcds_run -console -game open_fortress \
-		-insecure -timeout 0 -nohltv -autoupdate \
-		-steam_dir /root/server/ -steamcmd_script /root/tf2sdk-update.txt \
-		|| exit 1
-	echo "Server exited."
-	
-	echo "Updating Open Fortress"
-	until ~/of-update.sh
-	do
-		echo "Failed to update Open Fortress. Trying again in a five seconds."
-		sleep 5
-	done
+	echo "Failed to update Open Fortress. Trying again in a five seconds."
+	sleep 5
 done
+one
