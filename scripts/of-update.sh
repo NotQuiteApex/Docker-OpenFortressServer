0f1d2e3103b4b7d318e8218a8c69c06cc98798c2
Logan H-D <notquiteapex@gmail.com>
2022-06-08 21:56:18 -0400

    have murse verify subsequent builds

--- scripts/of-update.sh
+++ scripts/of-update.sh
@@ -9,5 +9,9 @@ mup () {
 	~/murse upgrade ~/server/sdk/open_fortress/ -u "$arg1"
 }
 
+vup () {
+	~/murse verify ~/server/sdk/open_fortress/ -u "$arg1" -r
+}
+
 # Exit if all the alt urls fail.
-{mup "${u[0]}" || mup "${u[1]}" || mup "${u[2]}"} || exit 1
+{mup "${u[0]}" || vup "${u[1]}" || vup "${u[2]}"} || exit 1
