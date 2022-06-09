c8d681808fda35ac100fedf1a9c43cbe4c979f4e
Logan H-D <notquiteapex@gmail.com>
2022-06-08 22:34:16 -0400

    use default url and only verify once (change later?)

--- scripts/of-update.sh
+++ scripts/of-update.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 # Updates Open Fortress.
-declare -a u=( \
-	"https://toast1.openfortress.fun/toast/" \
-	"https://toast2.openfortress.fun/toast/" \
-	"https://toast3.openfortress.fun/toast/" )
 
 mup () {
-	~/murse upgrade ~/server/sdk/open_fortress/ -u "$1"
+	~/murse upgrade ~/server/sdk/open_fortress/ -u "https://toast.openfortress.fun/toast/"
 }
 
 vup () {
-	~/murse verify ~/server/sdk/open_fortress/ -u "$1" -r
+	~/murse verify ~/server/sdk/open_fortress/ -r -u "https://toast.openfortress.fun/toast/"
 }
 
 # Exit if all the alt urls fail.
@@ -18,9 +14,7 @@ mup "${u[0]}"
 st0 = $?
 vup "${u[1]}"
 st1 = $?
-vup "${u[2]}"
-st2 = $?
 
-if [$st0 -ne 0] && [$st1 -ne 0] && [$st2 -ne 0]; then
+if [$st0 -ne 0] && [$st1 -ne 0]; then
 	exit 1
 fi
