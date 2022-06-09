61ad2e3d69b65fe481481e93948d51d86f8822a3
Logan H-D <notquiteapex@gmail.com>
2022-06-08 23:31:12 -0400

    optimize script a bit

--- scripts/of-update.sh
+++ scripts/of-update.sh
@@ -5,23 +5,15 @@ declare -a u=( \
 	"https://toast2.openfortress.fun/toast/" \
 	"https://toast3.openfortress.fun/toast/" )
 
-mup () {
-	~/murse upgrade ~/server/sdk/open_fortress/ -u "$1"
-}
-
-vup () {
-	~/murse verify ~/server/sdk/open_fortress/ -r -u "$1"
-}
-
 for str in ${u[@]}; do
 	echo "Checking toast link: $str"
-	mup "$str"
+	~/murse upgrade ~/server/sdk/open_fortress/ -u "$str"
 	if [ $? -ne 0 ]; then
 		echo "Toast link failed, trying next..."
 		continue
 	fi
 	echo "Verifying toast..."
-	vup "$str"
+	~/murse verify ~/server/sdk/open_fortress/ -r -u "$str"
 	if [ $? -ne 0 ]; then
 		echo "Toast verify failed, trying next..."
 		continue
@@ -30,5 +22,5 @@ for str in ${u[@]}; do
 	exit 0
 done
 
-echo "Failed to pull toast, failed..."
+echo "Failed to pull toast, exiting..."
 exit 1
