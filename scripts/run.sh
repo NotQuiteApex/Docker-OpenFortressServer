208999195a8b67c1a3672ca13e3bcd0907d319e5
Logan H-D <notquiteapex@gmail.com>
2022-06-11 14:58:34 -0400

    update gameinfo.txt and pull updates outside of srcds

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,16 +5,24 @@ while :
 do
 	echo "Starting Server."
 	./sdk/srcds_run -console -game open_fortress \
-		-secure -timeout 0 -nohltv -autoupdate \
-		-steam_dir /root/.steam/steamcmd/ \
-		-steamcmd_script ./tf2sdk-update.txt || \
+		-secure -timeout 0 -nohltv || \
 		exit 1
+
 	echo "Server exited."
+
+	echo "Updating TF2 + SDK."
+	until steamcmd +runscript ./tf2sdk-update.txt
+	do
+		echo "Failed to update TF2 + SDK. Trying again in a five seconds."
+		sleep 5
+	done
+	echo "TF2 + SDK update finished."
 	
-	echo "Updating Open Fortress"
+	echo "Updating Open Fortress."
 	until ./of-update.sh
 	do
 		echo "Failed to update Open Fortress. Trying again in a five seconds."
 		sleep 5
 	done
+	echo "Open Fortress update finished."
 done
