8720aca12fc2ac005b24f1690c38c0570a9542a3
Logan H-D <notquiteapex@gmail.com>
2023-02-09 16:01:45 -0500

    fix murse download issue (https2 -> 1)

--- scripts/of-update.sh
+++ scripts/of-update.sh
@@ -1,32 +1,26 @@
 #!/usr/bin/env bash
 # Updates Open Fortress.
-declare -a u=( \
-	"https://toast.openfortress.fun/toast" )
 
-for str in ${u[@]}; do
-	echo "Checking toast link: $str"
-	./murse upgrade ./sdk/open_fortress/ -u "$str"
-	if [ $? -ne 0 ]; then
-		echo "Toast link failed, trying next..."
-		continue
-	fi
-	echo "Download complete."
-	echo "Verifying toast..."
-	./murse verify ./sdk/open_fortress/ -r -u "$str"
-	if [ $? -ne 0 ]; then
-		echo "Toast verify failed, trying next..."
-		continue
-	fi
-	echo "Verify success."
+echo "Downloading Open Fortress via toast."
+./murse upgrade ./sdk/open_fortress/ -1
+if [ $? -ne 0 ]; then
+	echo "Toast link failed, trying next..."
+	continue
+fi
+echo "Download complete."
+echo "Verifying toast..."
+./murse verify ./sdk/open_fortress/ -r -1
+if [ $? -ne 0 ]; then
+	echo "Toast verify failed, trying next..."
+	continue
+fi
+echo "Verify success."
 
-	echo "Fixing gameinfo.txt"
-	sed -i 's+|all_source_engine_paths|..\\Team Fortress 2\\+/root/.steam/ofsv/tf2/+g' ./sdk/open_fortress/gameinfo.txt
+echo "Fixing gameinfo.txt"
+sed -i 's+|all_source_engine_paths|..\\Team Fortress 2\\+/root/.steam/ofsv/tf2/+g' ./sdk/open_fortress/gameinfo.txt
 
-	echo "Removing C++ binary"
-	rm ./sdk/bin/libstdc++.so.6
+echo "Removing C++ binary"
+rm ./sdk/bin/libstdc++.so.6
 
-	echo "Install complete, exiting."
-	exit 0
-done
-echo "Failed to pull toast, exiting..."
-exit 1
+echo "Install complete, exiting."
+exit 0
