e1238b394b802c0433a51ead25e41573429d4e01
Logan H-D <notquiteapex@gmail.com>
2023-08-04 20:49:49 -0400

    update to use beans instead of murse

--- scripts/of-update.sh
+++ scripts/of-update.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 # Updates Open Fortress.
 
-echo "Downloading Open Fortress via toast."
-./murse upgrade ./sdk/open_fortress/ -1
-if [ $? -ne 0 ]; then
-	echo "Toast link failed, trying next..."
-	continue
-fi
-echo "Download complete."
-echo "Verifying toast..."
-./murse verify ./sdk/open_fortress/ -r -1
-if [ $? -ne 0 ]; then
-	echo "Toast verify failed, trying next..."
-	continue
-fi
-echo "Verify success."
+echo "Downloading Open Fortress via beans."
+
+while :
+do
+    if [ -d "open_fortress" ]; then
+        echo "OF is installed. Updating."
+        ./beans --update ./sdk/open_fortress/
+        if [ $? -ne 0 ]; then
+            echo "Beans failed, retrying..."
+            continue
+        fi
+        echo "Beans update complete."
+		break
+    else
+        echo "OF is not installed. Instaling."
+        ./beans --install ./sdk/open_fortress/
+        if [ $? -ne 0 ]; then
+     	    echo "Beans failed, retrying..."
+     	    continue
+        fi
+        echo "Beans install complete."
+		break
+    fi
+done
 
 echo "Fixing gameinfo.txt"
 sed -i 's+|all_source_engine_paths|..\\Team Fortress 2\\+/root/.steam/ofsv/tf2/+g' ./sdk/open_fortress/gameinfo.txt
